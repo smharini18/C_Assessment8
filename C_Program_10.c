3dbecab23439fd72af60a5a723e8f34b859dc4cd
smharini18 <smharini18@gmail.com>
2026-06-25 23:58:29 +0530

    Add function to calculate sum of 2-digit even primes

---
+++ C_Program_10.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int disp_2digits_even_sum6()
+{
+int num=2,index,isprime,total=0;
+while(num<10)
+{
+index=2;
+isprime=1;
+while(index<num)
+{
+if(num%index==0)
+{
+isprime=0;
+}
+index++;
+}
+if(isprime==1)
+{
+total=total+1;
+}
+num++;
+}
+return total;
+}
+int main()
+{
+int result;
+result=disp_2digits_even_sum6();
+printf("%d",result);
+}
